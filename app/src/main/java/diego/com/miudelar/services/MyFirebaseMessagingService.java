352137658eec7d096032f76b9df753d01d9d3194
Diego Gutierrez <d.gutierrez.contacto@gmail.com>
2018-11-19 02:09:17 -0300

    Se modifican notificaciones: al pulsar la notificacion redirige hacia la actividad de curso o examen

--- app/src/main/java/diego/com/miudelar/services/MyFirebaseMessagingService.java
+++ app/src/main/java/diego/com/miudelar/services/MyFirebaseMessagingService.java
@@ -1,9 +1,24 @@
 package diego.com.miudelar.services;
 
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v7.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Random;
+
+import diego.com.miudelar.R;
+import diego.com.miudelar.activities.MainActivity;
+import diego.com.miudelar.activities.ResultadoCursos;
+import diego.com.miudelar.activities.ResultadoExamenes;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
     private static final String TAG = "FCM Service";
@@ -19,6 +34,39 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         // message, here is where that should be initiated.
         super.onMessageReceived(remoteMessage);
         Log.d(TAG, "From: " + remoteMessage.getFrom());
-        Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
+        //og.d(TAG, "Notification Message Body: " + remoteMessage.getData().getBody());
+
+        long[] pattern = {500,500,500,500,500};
+
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        if (remoteMessage.getData() != null) {
+
+            Intent intent;
+
+            if (remoteMessage.getData().get("tipo").equalsIgnoreCase("curso")) {
+                intent = new Intent(this, ResultadoCursos.class);
+            }
+            else {
+                intent = new Intent(this, ResultadoExamenes.class);
+            }
+
+            //Intent intent = new Intent(this, MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* request code */, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+            Notification notification = new NotificationCompat.Builder(this)
+                    .setContentTitle(remoteMessage.getData().get("title"))
+                    .setContentText(remoteMessage.getData().get("body"))
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setAutoCancel(true)
+                    .setVibrate(pattern)
+                    .setLights(Color.BLUE, 1, 1)
+                    .setSound(defaultSoundUri)
+                    .setContentIntent(pendingIntent)
+                    .build();
+            NotificationManagerCompat manager = NotificationManagerCompat.from(getApplicationContext());
+            manager.notify(new Random().nextInt(), notification);
+        }
     }
 }
